c91e2e4c6a6b4cd044b4f510e8272cc8c23f16c4
liulinru <liulinru@souche.com>
2020-07-01 17:58:36 +0800

    增加删除最近若干次提交记录的快捷命令git del

--- init.sh
+++ init.sh
@@ -8,4 +8,7 @@ git config --local alias.lg7 "log --graph --pretty=format:'[%h] [%s] [%an] [%cd]
 # 格式化最近一个月的提交
 git config --local alias.lg30 "log --graph --pretty=format:'[%h] [%s] [%an] [%cd]' --date=format:'%Y-%m-%d %H:%M:%S' --since=1.months"
 
+# 删除之前的几个提交记录 git del 1
+git config --local alias.del "!f() { git reset --hard HEAD~\$1; }; f"
+
 chmod 777 ./ci.sh
